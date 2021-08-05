877ad17f7034a2d4d7f8da8fd13df69770397c43
pescarualex <pescarualexflorin@gmail.com>
2021-08-05 11:52:27 +0300

    initial commit

---
+++ src/test/java/com/example/todolistspring/ToDoListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
